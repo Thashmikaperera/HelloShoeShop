6b7ae4b000dfa8a1be3a2f4248734be190819899
Thashmika Perera <pthashmika@gmail.com>
2024-04-25 10:41:10 +0530

    add entity classes

--- src/main/java/lk/ijse/helloshoeshop/controller/CustomerController.java
+++ src/main/java/lk/ijse/helloshoeshop/controller/CustomerController.java
@@ -1,4 +1,15 @@
 package lk.ijse.helloshoeshop.controller;
 
+import lk.ijse.helloshoeshop.service.CustomerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+@RestController
+@RequestMapping("api/v1/customer")
 public class CustomerController {
+
 }
---
+++ src/main/java/lk/ijse/helloshoeshop/entity/enumeratedData/Category.java
@@ -0,0 +1,4 @@
+package lk.ijse.helloshoeshop.entity.enumeratedData;
+
+public enum Category {
+}
